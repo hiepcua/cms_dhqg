4259a7b4206751f023fefcafc026ec8d7df88d1e
Tran Hiep <tranviethiepdz@gmail.com>
2020-07-07 11:45:45 +0700

    nothing

--- global/libs/gfinit.php
+++ global/libs/gfinit.php
@@ -2,6 +2,6 @@
 define('HOSTNAME','localhost');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','');
-define('DB_DATANAME','ott_anninh24');
+define('DB_DATANAME','cms_dhqg');
 ini_set('default_charset', 'utf-8');
 date_default_timezone_set("Asia/Ho_Chi_Minh");
